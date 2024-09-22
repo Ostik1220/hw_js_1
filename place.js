d57ec43c9a194b2a06c3c409739b06642be041bb
Ostik <117195748+Ostik1220@users.noreply.github.com>
2024-09-22 20:39:10 +0300

    save

---
+++ place.js
@@ -0,0 +1,30 @@
+const age = 14
+console.log(age)
+
+const myName = "Ostap"
+console.log(myName)
+
+const isStudent = 1
+console.log(Boolean(isStudent))
+
+const myString = "Мене не кличуть, я сам приходжу на менторські"
+console.log(myString)
+
+let myNumber = 7
+console.log(myNumber)
+myNumber += 10
+console.log(myNumber)
+
+const myNull = null
+console.log(myNull)
+
+let Name = prompt("Введіть Ім'я")
+console.log(Name)
+
+let confirmationMessage = confirm("Ви впевнені, що хочете продовжити?") ? "Дякую за підтвердження!" : "Дію відмінено!";
+alert(confirmationMessage);
+
+confirm("Увага! Ця дія є небезпечною.");
+alert(confirm("Ви впевнені, що хочете продовжити?") ? "Дякую за підтвердження!" : "Дію відмінено!");
+
+
