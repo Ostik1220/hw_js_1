d895e55ffeefd97d28fdc1e3186096b4dfe649fa
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-07-11 17:42:03 +0300

    фйціувкаепгшол

--- place.js
+++ place.js
@@ -7,24 +7,27 @@ console.log(myName)
 const isStudent = 1
 console.log(Boolean(isStudent))
 
-const myString = "Мене не кличуть, я сам приходжу на менторські"
-console.log(myString)
+// const myString = "Мене не кличуть, я сам приходжу на менторські"
+// console.log(myString)
 
-let myNumber = 7
-console.log(myNumber)
-myNumber += 10
-console.log(myNumber)
+// let myNumber = 7
+// console.log(myNumber)
+// myNumber += 10
+// console.log(myNumber)
 
-const myNull = null
-console.log(myNull)
+// const myNull = null
+// console.log(myNull)
 
-let Name = prompt("Введіть Ім'я")
-console.log(Name)
+// let Name = prompt("Введіть Ім'я")
+// console.log(Name)
 
-let confirmationMessage = confirm("Ви впевнені, що хочете продовжити?") ? "Дякую за підтвердження!" : "Дію відмінено!";
-alert(confirmationMessage);
+// let confirmationMessage = confirm("Ви впевнені, що хочете продовжити?") ? "Дякую за підтвердження!" : "Дію відмінено!";
+// alert(confirmationMessage);
 
-confirm("Увага! Ця дія є небезпечною.");
-alert(confirm("Ви впевнені, що хочете продовжити?") ? "Дякую за підтвердження!" : "Дію відмінено!");
+// confirm("Увага! Ця дія є небезпечною.");
+// alert(confirm("Ви впевнені, що хочете продовжити?") ? "Дякую за підтвердження!" : "Дію відмінено!");
 
 
+const b = 0o1101000; 
+
+console.log(b); 
